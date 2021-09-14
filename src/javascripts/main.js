afc01b69330922c973dae827824ae7460fa4c5ae
powderhound1976 <christopherreynolds@mail.weber.edu>
2021-09-13 21:21:44 -0600

    Inital Commit

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,21 +1,63 @@
 // Required by Webpack - do not touch
-require.context('../', true, /\.(html|json|txt|dat)$/i)
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
-require.context('../stylesheets/', true, /\.(css|scss)$/i)
+require.context('../', true, /\.(html|json|txt|dat)$/i);
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i);
+require.context('../stylesheets/', true, /\.(css|scss)$/i);
 
 //TODO - Your ES6 JavaScript code (if any) goes here
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-  render(){
-    return (
-      <h1>
-        Welcome to WEB 3430 
-        <span className="subtitle"> Powered by React!</span>
-      </h1>
-    )
-  }
+import { movies } from './movies';
+
+class Main extends React.Component {
+	render() {
+		return <MovieList movies= {movies} />;
+	}
 }
 
-ReactDOM.render(<Hello/>, document.getElementById('main'))
+class MovieList extends React.Component {
+	render() {
+		return (
+			<div className='container'>
+				<header>
+					<h1>Top 10 Movies</h1>
+				</header>
+				<nav>
+					<ul>
+						<li>Home</li>
+						<li>List</li>
+						<li>About</li>
+					</ul>
+				</nav>
+				<main>
+					{this.props.movies.map(m => {
+						return <Movie key={m.id} movie={m} />;
+					})}
+				</main>
+			</div>
+		);
+	}
+}
+
+function Movie(props) {
+	const m = props.movie;
+	return (
+		<div className='card'>
+			<img src={m.poster} alt={m.title} />
+			<h2>{m.title}</h2>
+			<p>{m.plot}</p>
+			<ul className='extra'>
+				<li>
+					<strong>{m.rating}</strong> rating
+				</li>
+				<li>
+					<strong>{m.votes}</strong> votes
+				</li>
+				<li>
+					<button className='primary'>Select</button>
+				</li>
+			</ul>
+		</div>
+	);
+}
+ReactDOM.render(<Main />, document.getElementById('main'));
